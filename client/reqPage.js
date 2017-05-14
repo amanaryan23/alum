4bf2a489a4ab981e37e73b5e61c06cc43c8a5da3
rohanjaswal2507 <rohanjaswal2507@gmail.com>
2017-05-15 05:13:24 +0530

    show shipping address in request page

--- client/reqPage.js
+++ client/reqPage.js
@@ -5,6 +5,17 @@ Template.reqPage.helpers({
     console.log(user);
     reqDetails['candidateName'] = user.firstName + " " + user.lastName;
     reqDetails['requestedOn'] = moment(reqDetails.createdAt).format('YYYY-MMM-DD  HH:MM:SS');
+    reqDetails['address'] = user.address;
     return reqDetails;
+  },
+});
+
+
+Template.reqPage.events({
+  'change .status' : function(){
+    console.log("status changed to");
+    var newStatus = $('[name=status_field]').val();
+    console.log(newStatus);
+    Requests.update({_id:Session.get('reqId')}, {$set :{status:newStatus}});
   }
 });
